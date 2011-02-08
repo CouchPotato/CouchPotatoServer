c8ba3ceb72ee2abf8c56b584142e062b5a364167
Ruud <ruud@crashdummy.nl>
2011-02-08 20:34:18 +0100

    Dependencies update

--- CouchPotato.py
+++ CouchPotato.py
@@ -16,35 +16,36 @@ except ImportError:
         cwd = dirname(__file__)
         print "Updating libraries..."
         stdout, stderr = subprocess.Popen(["git", "submodule", "init"],
-                                          stderr=subprocess.PIPE,
-                                          stdout=subprocess.PIPE).communicate()
+                                          stderr = subprocess.PIPE,
+                                          stdout = subprocess.PIPE).communicate()
         if stderr:
             print "[WARNING] Git is complaining:"
-            print "="*78
+            print "=" * 78
             print stderr
-            print "="*78
+            print "=" * 78
         stdout, stderr = subprocess.Popen(["git", "submodule", "update"],
-                                          stderr=subprocess.PIPE,
-                                          stdout=subprocess.PIPE).communicate()
+                                          stderr = subprocess.PIPE,
+                                          stdout = subprocess.PIPE).communicate()
         if stderr:
             print "[WARNING] Git is complaining:"
-            print "="*78
+            print "=" * 78
             print stderr
-            print "="*78
+            print "=" * 78
         print "Registering libraries..."
         # Insert local directories into path
         lib_path = os.path.join(os.path.abspath(cwd), 'libs')
         src_path = os.path.join(os.path.abspath(cwd), 'src')
         sys.path.insert(0, lib_path)
         sys.path.insert(0, src_path)
+        print sys.path
         print "Passing execution to couchpotato..."
         try:
             from couchpotato import cli
         except ImportError:
             print "[ERROR]: Something's seriously wrong."
-            print "="*78
+            print "=" * 78
             traceback.print_exc()
-            print "="*78
+            print "=" * 78
             print "Aborting..."
             sys.exit(1)
     else:
--- src/couchpotato/cli.py
+++ src/couchpotato/cli.py
@@ -1,5 +1,12 @@
+from flask import Flask
 import argparse
 
 def cmd_couchpotato():
-    print "Couchpotato."
-    pass
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    if __name__ == "__main__":
+        app.run(debug = True)
