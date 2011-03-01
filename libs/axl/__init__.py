fd5a41435b3a6b98554b8fc27959e5913381b643
Ruud <ruud@crashdummy.nl>
2011-03-01 11:01:59 +0100

    No need for import in __init__

--- libs/axl/__init__.py
+++ libs/axl/__init__.py
@@ -5,9 +5,3 @@
 # This module is part of Axel and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php 
 
-import inspect
-from .axel import *
-__all__ = sorted(name for name, obj in locals().items()
-                 if not (name.startswith('_') or inspect.ismodule(obj)))
-__all__.append('axel')
-del inspec
