4e4d47d6f8c49cf2b9c61560fc60d1b9dd265601
Ruud <ruud@crashdummy.nl>
2012-01-23 22:49:38 +0100

    toUnicode before stripaccents

--- couchpotato/core/helpers/encoding.py
+++ couchpotato/core/helpers/encoding.py
@@ -46,4 +46,4 @@ def isInt(value):
         return False
 
 def stripAccents(s):
-    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+    return ''.join((c for c in unicodedata.normalize('NFD', toUnicode(s)) if unicodedata.category(c) != 'Mn'))
