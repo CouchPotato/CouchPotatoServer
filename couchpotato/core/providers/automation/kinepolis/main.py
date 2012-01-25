636929347241a562e1163b704c58a23d93f56b45
Ruud <ruud@crashdummy.nl>
2012-01-25 15:46:15 +0100

    Automation kinepolis

--- couchpotato/core/providers/automation/kinepolis/main.py
+++ couchpotato/core/providers/automation/kinepolis/main.py
@@ -1,14 +1,41 @@
+from couchpotato.core.helpers.rss import RSS
 from couchpotato.core.logger import CPLog
 from couchpotato.core.providers.automation.base import Automation
+import xml.etree.ElementTree as XMLTree
 
 log = CPLog(__name__)
 
 
-class Kinepolis(Automation):
+class Kinepolis(Automation, RSS):
 
-    def getMovies(self):
+    urls = {
+        'top10': 'http://kinepolis.be/nl/top10-box-office/feed',
+    }
+
+
+    def getIMDBids(self):
 
         if self.isDisabled():
             return
 
-        return []
+        movies = []
+        for key in self.urls:
+            url = self.urls[key]
+            cache_key = 'kinepolis.%s' % key
+
+            rss_data = self.getCache(cache_key, url)
+            try:
+                items = self.getElements(XMLTree.fromstring(rss_data), 'channel/item')
+            except Exception, e:
+                log.debug('%s, %s' % (self.getName(), e))
+                continue
+
+            for item in items:
+                title = self.getTextElement(item, "title").lower()
+                result = self.search(title)
+
+                if result:
+                    if self.isMinimal(result):
+                        movies.append(result)
+
+        return movies
