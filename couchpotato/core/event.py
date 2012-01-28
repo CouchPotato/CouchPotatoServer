30ea948df885148dbe2a3cdf5ff26953d9363f13
Ruud <ruud@crashdummy.nl>
2012-01-28 12:32:35 +0100

    Typo

--- couchpotato/core/event.py
+++ couchpotato/core/event.py
@@ -12,7 +12,7 @@ def runHandler(name, handler, *args, **kwargs):
         return handler(*args, **kwargs)
     except:
         from couchpotato.environment import Env
-        log.error('Error in event "%s", that wasn\'nt caught: %s%s' % (name, traceback.format_exc(), Env.all()))
+        log.error('Error in event "%s", that wasn\'t caught: %s%s' % (name, traceback.format_exc(), Env.all()))
 
 def addEvent(name, handler, priority = 100):
 
