6b739ee52c38c37294b03d15de636c78ede8be53
Ruud <ruud@crashdummy.nl>
2012-04-06 23:28:04 +0200

    Extra br50 tags

--- couchpotato/core/plugins/quality/main.py
+++ couchpotato/core/plugins/quality/main.py
@@ -16,7 +16,7 @@ log = CPLog(__name__)
 class QualityPlugin(Plugin):
 
     qualities = [
-        {'identifier': 'bd50', 'hd': True, 'size': (15000, 60000), 'label': 'BR-Disk', 'width': 1920, 'alternative': ['bd25'], 'allow': ['1080p'], 'ext':[], 'tags': ['bdmv', 'certificate']},
+        {'identifier': 'bd50', 'hd': True, 'size': (15000, 60000), 'label': 'BR-Disk', 'width': 1920, 'alternative': ['bd25'], 'allow': ['1080p'], 'ext':[], 'tags': ['bdmv', 'certificate', ('complete', 'bluray')]},
         {'identifier': '1080p', 'hd': True, 'size': (5000, 20000), 'label': '1080P', 'width': 1920, 'alternative': [], 'allow': [], 'ext':['mkv', 'm2ts']},
         {'identifier': '720p', 'hd': True, 'size': (3500, 10000), 'label': '720P', 'width': 1280, 'alternative': [], 'allow': [], 'ext':['mkv', 'm2ts']},
         {'identifier': 'brrip', 'hd': True, 'size': (700, 7000), 'label': 'BR-Rip', 'alternative': ['bdrip'], 'allow': ['720p'], 'ext':['avi']},
@@ -154,6 +154,10 @@ class QualityPlugin(Plugin):
                 if list(set(quality.get('alternative', [])) & set(words)):
                     log.debug('Found %s via alt %s in %s' % (quality['identifier'], quality.get('alternative'), cur_file))
                     return self.setCache(hash, quality)
+
+                for tag in quality.get('tags', []):
+                    if isinstance(tag, tuple) and '.'.join(tag) in '.'.join(words):
+                        return self.setCache(hash, quality)
 
                 if list(set(quality.get('tags', [])) & set(words)):
                     log.debug('Found %s via tag %s in %s' % (quality['identifier'], quality.get('tags'), cur_file))
