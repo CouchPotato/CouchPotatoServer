d4a5483fa034d960bb7160a076ee11507e3bdc78
Ruud Burger <ruud@crashdummy.nl>
2012-04-26 10:57:34 +0300

    Use master branch to update master..

--- couchpotato/core/_base/updater/main.py
+++ couchpotato/core/_base/updater/main.py
@@ -80,7 +80,7 @@ class BaseUpdater(Plugin):
 
     repo_user = 'RuudBurger'
     repo_name = 'CouchPotatoServer'
-    branch = 'develop'
+    branch = 'master'
 
     version = None
     update_failed = False
