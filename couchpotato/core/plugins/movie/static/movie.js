15c76bf7cb3f14470b22b84cef9b28e200b45008
Ruud <ruud@crashdummy.nl>
2012-06-16 17:45:57 +0200

    Trailer support

--- couchpotato/core/plugins/movie/static/movie.js
+++ couchpotato/core/plugins/movie/static/movie.js
@@ -77,7 +77,7 @@ var Movie = new Class({
 
 		self.profile = Quality.getProfile(self.data.profile_id) || {};
 		self.create();
-		
+
 		self.busy(false);
 	},
 
@@ -246,6 +246,10 @@ var Movie = new Class({
 
 	isSelected: function(){
 		return this.select_checkbox.get('checked');
+	},
+
+	toElement: function(){
+		return this.el;
 	}
 
 });
@@ -424,5 +428,110 @@ var ReleaseAction = new Class({
 		})
 
 	}
+
+});
+
+var TrailerAction = new Class({
+
+	Extends: MovieAction,
+	id: null,
+
+	create: function(){
+		var self = this;
+
+		self.el = new Element('a.trailer', {
+			'title': 'Watch the trailer of ' + self.movie.getTitle(),
+			'events': {
+				'click': self.watch.bind(self)
+			}
+		});
+
+	},
+
+	watch: function(offset){
+		var self = this;
+
+		var data_url = 'http://gdata.youtube.com/feeds/videos?vq="{title}" {year} trailer&max-results=1&alt=json-in-script&orderby=relevance&sortorder=descending&format=5&fmt=18'
+		var url = data_url.substitute({
+				'title': self.movie.getTitle(),
+				'year': self.movie.get('year'),
+				'offset': offset || 1
+			}),
+			size = $(self.movie).getSize(),
+			height = (size.x/16)*9,
+			id = 'trailer-'+randomString();
+
+		self.player_container = new Element('div[id='+id+']');
+		self.container = new Element('div.hide.trailer_container')
+			.adopt(self.player_container)
+			.inject(self.movie.container, 'top');
+
+		self.container.setStyle('height', 0);
+		self.container.removeClass('hide');
+
+		self.close_button = new Element('a.hide.hide_trailer', {
+			'text': 'Hide trailer',
+			'events': {
+				'click': self.stop.bind(self)
+			}
+		}).inject(self.movie);
+
+		setTimeout(function(){
+			$(self.movie).setStyle('max-height', height);
+			self.container.setStyle('height', height);
+		}, 100)
+
+		new Request.JSONP({
+			'url': url,
+			'onComplete': function(json){
+				var video_url = json.feed.entry[0].id.$t.split('/'),
+					video_id = video_url[video_url.length-1];
+
+				self.player = new YT.Player(id, {
+					'height': height,
+					'width': size.x,
+					'videoId': video_id,
+					'playerVars': {
+						'autoplay': 1,
+						'showsearch': 0,
+						'wmode': 'transparent',
+						'iv_load_policy': 3
+					}
+				});
+
+				self.close_button.removeClass('hide');
+
+				var quality_set = false;
+				var change_quality = function(state){
+					if(!quality_set && (state.data == 1 || state.data || 2)){
+						try {
+							self.player.setPlaybackQuality('hd720');
+							quality_set = true;
+						}
+						catch(e){
+
+						}
+					}
+				}
+				self.player.addEventListener('onStateChange', change_quality);
+
+			}
+		}).send()
+
+	},
+
+	stop: function(){
+		var self = this;
+
+		self.player.stopVideo();
+		self.container.addClass('hide');
+		self.close_button.addClass('hide');
+
+		setTimeout(function(){
+			self.container.destroy()
+			self.close_button.destroy();
+		}, 1800)
+	}
+
 
 });
