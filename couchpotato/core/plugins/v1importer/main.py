61524da053740dc172459bf02cad19e64d578b47
xbgmsharp <xbgmsharp@gmail.com>
2012-06-19 15:16:21 +0200

    Added: Importe all movie from v1 database, not only want's movies

--- couchpotato/core/plugins/v1importer/main.py
+++ couchpotato/core/plugins/v1importer/main.py
@@ -1,5 +1,5 @@
 from couchpotato.api import addApiView
-from couchpotato.core.event import fireEventAsync
+from couchpotato.core.event import fireEvent, fireEventAsync, addEvent
 from couchpotato.core.helpers.variable import getImdb
 from couchpotato.core.logger import CPLog
 from couchpotato.core.plugins.base import Plugin
@@ -41,14 +41,78 @@ class V1Importer(Plugin):
             for row in cur:
                 status, imdb = row
                 if getImdb(imdb):
-                    wanted.append(imdb)
-            conn.close()
+                   wanted.append(imdb)
 
             wanted = set(wanted)
             for imdb in wanted:
                 fireEventAsync('movie.add', {'identifier': imdb}, search_after = False)
 
-            message = 'Successfully imported %s movie(s)' % len(wanted)
+            message = 'Successfully imported %s wanted movie(s)' % len(wanted)
+
+
+            downloaded = []
+
+            t = ('downloaded',)
+            cur = conn.execute('SELECT status, imdb FROM Movie WHERE status=?', t)
+            for row in cur:
+                status, imdb = row
+                if getImdb(imdb):
+                    downloaded.append(imdb)
+
+            downloaded = set(downloaded)
+            for imdb in downloaded:
+		fireEvent('movie.add', {'identifier': imdb, status: 'done'}, single = True, search_after = False)
+                #fireEventAsync('movie.add', {'identifier': imdb, status: 'downloaded'}, search_after = False)
+
+            message = 'Successfully imported %s downloaded movie(s)' % len(wanted)
+
+            deleted = []
+
+            t = ('deleted',)
+            cur = conn.execute('SELECT status, imdb FROM Movie WHERE status=?', t)
+            for row in cur:
+                status, imdb = row
+                if getImdb(imdb):
+                    deleted.append(imdb)
+
+            deleted = set(deleted)
+            for imdb in deleted:
+		fireEvent('movie.add', {'identifier': imdb, status: 'deleted'}, single = True, search_after = False)
+                #fireEventAsync('movie.add', {'identifier': imdb, status: 'deleted'}, search_after = False)
+
+            message = 'Successfully imported %s deleted movie(s)' % len(wanted)
+
+            viewed = []
+
+            t = ('viewed',)
+            cur = conn.execute('SELECT status, imdb FROM Movie WHERE status=?', t)
+            for row in cur:
+                status, imdb = row
+                if getImdb(imdb):
+                    viewed.append(imdb)
+
+            viewed = set(viewed)
+            for imdb in viewed:
+                fireEventAsync('movie.add', {'identifier': imdb, status: 'viewed'}, search_after = False)
+
+            message = 'Successfully imported %s viewed movie(s)' % len(wanted)
+
+            snatched = []
+
+            t = ('snatched',)
+            cur = conn.execute('SELECT status, imdb FROM Movie WHERE status=?', t)
+            for row in cur:
+                status, imdb = row
+                if getImdb(imdb):
+                    snatched.append(imdb)
+            conn.close()
+
+            snatched = set(snatched)
+            for imdb in snatched:
+		fireEvent('movie.add', {'identifier': imdb, status: 'snatched'}, single = True, search_after = False)
+
+            message = 'Successfully imported %s snatched movie(s)' % len(wanted)
+
         except Exception, e:
             message = 'Failed: %s' % e
 
