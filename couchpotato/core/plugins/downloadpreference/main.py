23918114806a24cbfa5023ea857618448ba3b9eb
bwq <enjoi.444@gmail.com>
2012-06-27 20:32:14 +0200

    Optimized imports.

--- couchpotato/core/plugins/downloadpreference/main.py
+++ couchpotato/core/plugins/downloadpreference/main.py
@@ -1,16 +1,8 @@
-from couchpotato import get_session
 from couchpotato.api import addApiView
-from couchpotato.core.event import fireEvent, fireEventAsync, addEvent
-from couchpotato.core.helpers.encoding import toUnicode, simplifyString
-from couchpotato.core.helpers.request import getParams, jsonified, getParam
-from couchpotato.core.helpers.variable import getImdb
+from couchpotato.core.event import fireEvent, addEvent
+from couchpotato.core.helpers.request import jsonified
 from couchpotato.core.logger import CPLog
 from couchpotato.core.plugins.base import Plugin
-from couchpotato.core.settings.model import Library, LibraryTitle, Movie
-from couchpotato.environment import Env
-from sqlalchemy.orm import joinedload_all
-from sqlalchemy.sql.expression import or_, asc, not_
-from string import ascii_lowercase
 
 log = CPLog(__name__)
 
