4d90b1873b829b7609ddfc9afdd95a3f2b9896f0
Ruud <ruud@crashdummy.nl>
2012-08-09 22:23:09 +0200

    Add SD documentary category NZBMatrix. fix #698

--- couchpotato/core/providers/nzb/nzbmatrix/main.py
+++ couchpotato/core/providers/nzb/nzbmatrix/main.py
@@ -22,7 +22,7 @@ class NZBMatrix(NZBProvider, RSS):
     cat_ids = [
         ([50], ['bd50']),
         ([42, 53], ['720p', '1080p']),
-        ([2], ['cam', 'ts', 'dvdrip', 'tc', 'r5', 'scr']),
+        ([2, 9], ['cam', 'ts', 'dvdrip', 'tc', 'r5', 'scr']),
         ([54], ['brrip']),
         ([1], ['dvdr']),
     ]
