5fe645cc110bd6d10e5ef0421d351006bff0f545
Ruud <ruud@crashdummy.nl>
2012-11-03 21:47:37 +0100

    Return boolean properly

--- couchpotato/core/settings/__init__.py
+++ couchpotato/core/settings/__init__.py
@@ -90,7 +90,7 @@ class Settings(object):
     def set(self, section, option, value):
         return self.p.set(section, option, value)
 
-    def get(self, option = '', section = 'core', default = '', type = None):
+    def get(self, option = '', section = 'core', default = None, type = None):
         try:
 
             try: type = self.types[section][option]
@@ -111,7 +111,7 @@ class Settings(object):
         try:
             return self.p.getboolean(section, option)
         except:
-            return self.p.get(section, option)
+            return self.p.get(section, option) == 1
 
     def getInt(self, section, option):
         try:
