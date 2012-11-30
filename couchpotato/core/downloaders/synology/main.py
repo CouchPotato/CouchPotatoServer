618845a0219433b521a64781a914334e1fd1d347
Ruud <ruud@crashdummy.nl>
2012-11-30 22:37:23 +0100

    Code formatting

--- couchpotato/core/downloaders/synology/main.py
+++ couchpotato/core/downloaders/synology/main.py
@@ -3,9 +3,9 @@ from couchpotato.core.helpers.encoding import isInt
 from couchpotato.core.logger import CPLog
 import httplib
 import json
-
-import urllib2
 import urllib
+import urllib2
+
 
 log = CPLog(__name__)
 
@@ -35,35 +35,35 @@ class Synology(Downloader):
             # Send request to Transmission
             srpc = SynologyRPC(host[0], host[1], self.conf('username'), self.conf('password'))
             remote_torrent = srpc.add_torrent_uri(data.get('url'))
-            log.info("Response: %s", remote_torrent)
+            log.info('Response: %s', remote_torrent)
             return remote_torrent['success']
         except Exception, err:
-            log.error("Exception while adding torrent: %s", err)
+            log.error('Exception while adding torrent: %s', err)
             return False
 
 
 class SynologyRPC(object):
 
-    """SynologyRPC lite library"""
+    '''SynologyRPC lite library'''
 
     def __init__(self, host = 'localhost', port = 5000, username = None, password = None):
 
         super(SynologyRPC, self).__init__()
 
-        self.DLurl    = 'http://' + host + ':' + str(port) + '/webapi/DownloadStation/task.cgi'
-        self.Authurl  = 'http://' + host + ':' + str(port) + '/webapi/auth.cgi'
+        self.download_url = 'http://%s:%s/webapi/DownloadStation/task.cgi' % (host, port)
+        self.auth_url = 'http://%s:%s/webapi/auth.cgi' % (host, port)
         self.username = username
         self.password = password
-        self.session_name = "DownloadStation"
+        self.session_name = 'DownloadStation'
 
     def _login(self):
         if self.username and self.password:
             args = {'api': 'SYNO.API.Auth', 'account': self.username, 'passwd': self.password, 'version': 2,
             'method': 'login', 'session': self.session_name, 'format': 'sid'}
-            response = self._req(self.Authurl, args)
+            response = self._req(self.auth_url, args)
             if response['success'] == True:
                 self.sid = response['data']['sid']
-                log.debug( "Sid=%s", self.sid)
+                log.debug('Sid=%s', self.sid)
             return response
         elif self.username or self.password:
             log.error('User or password missing, not using authentication.')
@@ -71,7 +71,7 @@ class SynologyRPC(object):
 
     def _logout(self):
         args = {'api':'SYNO.API.Auth', 'version':1, 'method':'logout', 'session':self.session_name, '_sid':self.sid}
-        return self._req(self.Authurl, args)
+        return self._req(self.auth_url, args)
 
     def _req(self, url, args):
         req_url = url + '?' + urllib.urlencode(args)
@@ -92,17 +92,17 @@ class SynologyRPC(object):
             return False
 
     def add_torrent_uri(self, torrent):
-        log.info( "Adding torrent URL %s", torrent)
+        log.info('Adding torrent URL %s', torrent)
         response = {}
         # login
         login = self._login()
         if len(login) > 0 and login['success'] == True:
-            log.info("Login success, adding torrent")
+            log.info('Login success, adding torrent')
             args = {'api':'SYNO.DownloadStation.Task', 'version':1, 'method':'create', 'uri':torrent, '_sid':self.sid}
-            response = self._req(self.DLurl, args)
+            response = self._req(self.download_url, args)
             self._logout()
         else:
-            log.error("Couldn't login to Synology, %s", login)
+            log.error('Couldn\'t login to Synology, %s', login)
         return response
 
 
