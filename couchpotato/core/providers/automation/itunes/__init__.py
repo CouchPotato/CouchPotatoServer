637b21cc68b6c62deb9c38271adcbd43627155f9
Ruud <ruud@crashdummy.nl>
2013-01-04 20:20:52 +0100

    iTunes automation cleanup

--- couchpotato/core/providers/automation/itunes/__init__.py
+++ couchpotato/core/providers/automation/itunes/__init__.py
@@ -1,4 +1,4 @@
-from .main import ITunes 
+from .main import ITunes
 
 def start():
     return ITunes()
--- couchpotato/core/providers/automation/itunes/main.py
+++ couchpotato/core/providers/automation/itunes/main.py
@@ -1,11 +1,11 @@
 from couchpotato.core.helpers.rss import RSS
-from couchpotato.core.helpers.variable import md5, getImdb, splitString, tryInt
+from couchpotato.core.helpers.variable import md5, splitString, tryInt
 from couchpotato.core.logger import CPLog
 from couchpotato.core.providers.automation.base import Automation
-import datetime
-import xml.etree.ElementTree as XMLTree
 from xml.etree.ElementTree import QName
+import datetime
 import traceback
+import xml.etree.ElementTree as XMLTree
 
 log = CPLog(__name__)
 
@@ -26,7 +26,7 @@ class ITunes(Automation, RSS):
 
         namespace = 'http://www.w3.org/2005/Atom'
         namespaceIM = 'http://itunes.apple.com/rss'
-        
+
         index = -1
         for url in urls:
 
@@ -39,24 +39,24 @@ class ITunes(Automation, RSS):
                 rss_data = self.getCache(cache_key, url)
 
                 data = XMLTree.fromstring(rss_data)
-                
+
                 if data is not None:
                     entry_tag = str(QName(namespace, 'entry'))
                     rss_movies = self.getElements(data, entry_tag)
-                    
+
                     for movie in rss_movies:
                         name_tag = str(QName(namespaceIM, 'name'))
-                        name = self.getTextElement( movie, name_tag )
-                        
-                        releaseDate_tag = str(QName(namespaceIM, 'releaseDate')) 
+                        name = self.getTextElement(movie, name_tag)
+
+                        releaseDate_tag = str(QName(namespaceIM, 'releaseDate'))
                         releaseDateText = self.getTextElement(movie, releaseDate_tag)
                         year = datetime.datetime.strptime(releaseDateText, '%Y-%m-%dT00:00:00-07:00').strftime("%Y")
-                        
+
                         imdb = self.search(name, year)
-                        
+
                         if imdb and self.isMinimalMovie(imdb):
                             movies.append(imdb['imdb'])
-            
+
             except:
                 log.error('Failed loading iTunes rss feed: %s %s', (url, traceback.format_exc()))
 
