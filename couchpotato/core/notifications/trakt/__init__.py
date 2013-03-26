e350b8b4a01187e5daad615c2685b75e561e9a44
Sarakha63 <sarakha_ludovic@yahoo.fr>
2013-03-26 22:03:16 +0100

    finally finished

---
+++ couchpotato/core/notifications/trakt/__init__.py
@@ -0,0 +1,30 @@
+from .main import Trakt
+
+def start():
+    return Trakt()
+
+config = [{
+    'name': 'trakt',
+    'groups': [
+        {
+            'tab': 'notifications',
+            'list': 'notification_providers',
+            'name': 'trakt',
+            'label': 'Trakt',
+            'description': 'add movies to your collection once downloaded. Fill in your username and password in the <a href="../automation/">Automation Trakt settings</a>',
+            'options': [
+                {
+                    'name': 'notification_enabled',
+                    'default': False,
+                    'type': 'enabler',
+                },
+                {
+                    'name': 'remove_watchlist_enabled',
+                    'label': 'Remove from watchlist',
+                    'default': False,
+                    'type': 'bool',
+                },
+            ],
+        }
+    ],
+}]
---
+++ couchpotato/core/providers/automation/letterboxd/main.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from couchpotato.core.helpers.variable import tryInt, splitString
+from couchpotato.core.logger import CPLog
+from couchpotato.core.providers.automation.base import Automation
+import re
+
+log = CPLog(__name__)
+
+
+class Letterboxd(Automation):
+
+    url = 'http://letterboxd.com/%s/watchlist/'
+    pattern = re.compile(r'(.*)\((\d*)\)')
+
+    def getIMDBids(self):
+
+        urls = splitString(self.conf('automation_urls'))
+
+        if len(urls) == 0:
+            return []
+
+        movies = []
+
+        for movie in self.getWatchlist():
+            imdb_id = self.search(movie.get('title'), movie.get('year'), imdb_only = True)
+            movies.append(imdb_id)
+
+        return movies
+
+    def getWatchlist(self):
+
+        enablers = [tryInt(x) for x in splitString(self.conf('automation_urls_use'))]
+        urls = splitString(self.conf('automation_urls'))
+
+        index = -1
+        movies = []
+        for username in urls:
+
+            index += 1
+            if not enablers[index]:
+                continue
+
+            soup = BeautifulSoup(self.getHTMLData(self.url % username))
+
+            for movie in soup.find_all('a', attrs = { 'class': 'frame' }):
+                match = filter(None, self.pattern.split(movie['title']))
+                movies.append({'title': match[0], 'year': match[1] })
+
+        return movies
