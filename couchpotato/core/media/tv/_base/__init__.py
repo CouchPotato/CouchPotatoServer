c8d79cde211f565e585d9327543940263a60e853
Ruud <ruud@crashdummy.nl>
2013-08-15 23:47:07 +0200

    Add base for TV media type

---
+++ couchpotato/core/media/tv/_base/__init__.py
@@ -0,0 +1,6 @@
+from .main import TVBase
+
+def start():
+    return TVBase()
+
+config = []
---
+++ couchpotato/core/media/tv/searcher/__init__.py
@@ -0,0 +1,7 @@
+from .main import TVSearcher
+import random
+
+def start():
+    return TVSearcher()
+
+config = []
