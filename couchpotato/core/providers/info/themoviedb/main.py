56f63a1af5f03c18c3d178f34531135fdd661dfc
Shatil Rafiullah <shatil@teamtil.com>
2013-11-13 23:22:48 -0800

    Changed to using getattr() so films lacking sets/collections are also handled.

--- couchpotato/core/providers/info/themoviedb/main.py
+++ couchpotato/core/providers/info/themoviedb/main.py
@@ -121,7 +121,7 @@ class TheMovieDb(MovieProvider):
                 'year': year,
                 'plot': movie.overview,
                 'genres': genres,
-                'collection': movie.collection.name,
+                'collection': getattr(movie.collection, 'name', None),
             }
 
             movie_data = dict((k, v) for k, v in movie_data.iteritems() if v)
