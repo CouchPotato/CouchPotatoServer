b5d4056a3f26393a30e7d8d9a59ef08b6d8bd6e6
cptjhmiller <cpt.miller@fun4gamers.co.uk>
2013-11-21 11:03:44 +0000

    Added HDTorrents

---
+++ couchpotato/core/providers/torrent/hdtorrents/__init__.py
@@ -0,0 +1,54 @@
+from .main import HDTorrents
+
+def start():
+    return HDTorrents()
+
+config = [{
+    'name': 'hdtorrents',
+    'groups': [
+        {
+            'tab': 'searcher',
+            'list': 'torrent_providers',
+            'name': 'HDTorrents',
+            'description': 'See <a href="https://hdts.ru">HDTorrents</a>',
+            'options': [
+                {
+                    'name': 'enabled',
+                    'type': 'enabler',
+                    'default': False,
+                },
+                {
+                    'name': 'username',
+                    'default': '',
+                },
+                {
+                    'name': 'password',
+                    'default': '',
+                    'type': 'password',
+                },
+                {
+                    'name': 'seed_ratio',
+                    'label': 'Seed ratio',
+                    'type': 'float',
+                    'default': 1,
+                    'description': 'Will not be (re)moved until this seed ratio is met.',
+                },
+                {
+                    'name': 'seed_time',
+                    'label': 'Seed time',
+                    'type': 'int',
+                    'default': 40,
+                    'description': 'Will not be (re)moved until this seed time (in hours) is met.',
+                },
+                {
+                    'name': 'extra_score',
+                    'advanced': True,
+                    'label': 'Extra Score',
+                    'type': 'int',
+                    'default': 0,
+                    'description': 'Starting score for each release found via this provider.',
+                },
+            ],
+        },
+    ],
+}]
