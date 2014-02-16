4831c805986570d77613ea265dacd6dadd008e83
Ruud <ruud@crashdummy.nl>
2014-02-16 09:59:37 +0100

    Update nzbclub url

--- couchpotato/core/providers/nzb/nzbclub/main.py
+++ couchpotato/core/providers/nzb/nzbclub/main.py
@@ -13,7 +13,7 @@ log = CPLog(__name__)
 class NZBClub(NZBProvider, RSS):
 
     urls = {
-        'search': 'http://www.nzbclub.com/nzbfeed.aspx?%s',
+        'search': 'http://www.nzbclub.com/nzbfeeds.aspx?%s',
     }
 
     http_time_between_calls = 4  #seconds
