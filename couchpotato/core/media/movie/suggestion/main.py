00419910b49056d5f2a0e8a21fcf034574f8f300
Ruud <ruud@crashdummy.nl>
2014-03-20 16:54:54 +0100

    Use getIdentifier in suggestions

--- couchpotato/core/media/movie/suggestion/main.py
+++ couchpotato/core/media/movie/suggestion/main.py
@@ -1,7 +1,7 @@
 from couchpotato import get_db
 from couchpotato.api import addApiView
 from couchpotato.core.event import fireEvent
-from couchpotato.core.helpers.variable import splitString, removeDuplicate
+from couchpotato.core.helpers.variable import splitString, removeDuplicate, getIdentifier
 from couchpotato.core.plugins.base import Plugin
 from couchpotato.environment import Env
 
@@ -87,7 +87,7 @@ class Suggestion(Plugin):
         if len(new_suggestions) - 1 < limit:
             db = get_db()
             active_movies = db.run('media', 'with_status', ['active', 'done'])
-            movies = [x['identifier'] for x in active_movies]
+            movies = [getIdentifier(x) for x in active_movies]
             movies.extend(seen)
 
             ignored.extend([x.get('imdb') for x in cached_suggestion])
