e8dcf5ee0208d2f086d2b73ec37a095c9a23c030
bazbjzy <torrentmale1@gmail.com>
2014-03-31 17:59:14 -0700

    Added ability to configure Pushover custom sounds.

--- couchpotato/core/notifications/pushover.py
+++ couchpotato/core/notifications/pushover.py
@@ -25,6 +25,7 @@ class Pushover(Notification):
             'token': self.app_token,
             'message': toUnicode(message),
             'priority': self.conf('priority'),
+            'sound': self.conf('sound'),
         }
 
         if data and data.get('identifier'):
@@ -82,6 +83,12 @@ config = [{
                     'advanced': True,
                     'description': 'Also send message when movie is snatched.',
                 },
+                {
+                    'name': 'sound',
+                    'advanced': True,
+                    'description': 'Define <a href="https://pushover.net/api%23sounds" target="_blank">custom sound</a> for Pushover alert.'
+                },
+
             ],
         }
     ],
