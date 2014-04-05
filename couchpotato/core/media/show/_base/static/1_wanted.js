ae3d9c0a0a9ca2ef8911068dd743d0c0d8158731
Ruud <ruud@crashdummy.nl>
2014-04-05 11:51:55 +0200

    Add wanted shows

---
+++ couchpotato/core/media/show/_base/static/1_wanted.js
@@ -0,0 +1,28 @@
+Page.Shows = new Class({
+
+	Extends: PageBase,
+
+	name: 'shows',
+	title: 'Gimmy gimmy gimmy!',
+	folder_browser: null,
+
+	indexAction: function(){
+		var self = this;
+
+		if(!self.wanted){
+
+			// Wanted movies
+			self.wanted = new MovieList({
+				'identifier': 'wanted',
+				'status': 'active',
+				'type': 'show',
+				'actions': [MA.IMDB, MA.Trailer, MA.Release, MA.Edit, MA.Refresh, MA.Readd, MA.Delete],
+				'add_new': true,
+				'on_empty_element': App.createUserscriptButtons().addClass('empty_wanted')
+			});
+			$(self.wanted).inject(self.el);
+		}
+
+	}
+
+});
