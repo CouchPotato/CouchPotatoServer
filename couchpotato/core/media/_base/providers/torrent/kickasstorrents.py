828986ee64705d509daf90f2edd048f90c9029c8
Michael Walker <MLWALK3R@users.noreply.github.com>
2014-05-20 12:20:32 +0100

    Update kickasstorrents.py

--- couchpotato/core/media/_base/providers/torrent/kickasstorrents.py
+++ couchpotato/core/media/_base/providers/torrent/kickasstorrents.py
@@ -32,6 +32,8 @@ class Base(TorrentMagnetProvider):
     proxy_list = [
         'http://kickassto.come.in',
         'http://katproxy.ws',
+        'http://www.kickassunblock.info',
+        'http://www.kickassproxy.info',
         'http://katph.eu',
         'http://kickassto.come.in',
     ]
