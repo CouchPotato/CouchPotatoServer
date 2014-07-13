4291e2233d8f9918805dc42469489ad3d630eccb
Ruud <ruud@crashdummy.nl>
2014-07-13 12:23:27 +0200

    Releader class

---
+++ couchpotato/static/scripts/reloader.js
@@ -0,0 +1,24 @@
+var ReloaderBase = new Class({
+
+	initialize: function(){
+		var self = this;
+
+		App.on('watcher.changed', self.reloadFile.bind(self))
+
+	},
+
+	reloadFile: function(data){
+		var self = this,
+			urls = data.data;
+
+		urls.each(function(url){
+			var without_timestamp = url.split('?')[0],
+				old_links = document.getElement('[data-url^=\''+without_timestamp+'\']');
+
+			old_links.set('href', old_links.get('href') + 1)
+		})
+	}
+
+});
+
+var Reloader = new ReloaderBase();
