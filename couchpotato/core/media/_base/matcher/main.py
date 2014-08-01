9744e13c7f1d415c7aa5820878835562647b9462
seedzero <seedzero@users.noreply.github.com>
2014-08-01 20:49:17 +1000

    Removed temp log messages

--- couchpotato/core/media/_base/matcher/main.py
+++ couchpotato/core/media/_base/matcher/main.py
@@ -72,7 +72,6 @@ class Matcher(MatcherBase):
         return False
 
     def correctQuality(self, chain, quality):
-        log.info('quality: %s', quality['identifier'])
         release_video_info = {}
         for link in chain.info['video']: 
             release_video_info.update(link)
@@ -119,65 +118,52 @@ class Matcher(MatcherBase):
         if checkQuality(release_video_info, 'source', ['pdtv']):
             pdtv = True
 
-        # Temporary, remove the following line when finished:
-        log.info('SD:%s HD: %s, interlaced: %s, pdtv: %s, r720: %s, r1080: %s, rip %s, tv %s, webrip: %s', (sd, hd, interlaced, pdtv, r720, r1080, rip, tv, webrip))
-
         # sdtv
         if tv and (codec in ['h264', ['h', '264'], 'xvid', 'x264']) and not hd:
-            log.info('sdtv')
             if quality['identifier'] == 'sdtv':
                 return True
 
         elif tv and (codec in ['xvid', 'x264']) and not hd and not pdtv:
-            log.info('sdtv2')
             if quality['identifier'] == 'sdtv':
                 return True
 
         # sd_dvd
         elif (rip or (codec in ['xvid', 'divx', 'x264'])) and not hd:
-            log.info('sd_dvd')
             if quality['identifier'] == 'sd_dvd':
                 return True
         
         # hdtv
         elif r720 and tv and (codec == 'x264') and not webrip and not pdtv:
-            log.info('hdtv')
             if quality['identifier'] == 'hdtv':
                 return True
 
         # raw_hdtv
         elif ((r720 or (r1080 and interlaced)) and (codec in ['mpeg2', ['mpeg', '2']])) or (r1080 and tv and codec in ['h264', ['h', '264']]):
-            log.info('raw_hdtv')
             if quality['identifier'] == 'raw_hdtv':
                 return True
         
         # hdtv_1080p
         elif r1080 and not interlaced and tv and (codec == 'x264'):
-            log.info('hdtv_1080p')
             if quality['identifier'] == 'hdtv_1080p':
                 return True
         
         # webdl_720p
         elif r720 and webrip and not interlaced:
-            log.info('webdl_720p')
             if quality['identifier'] == 'webdl_720p':
                 return True
 
         # webdl_1080p
         elif r1080 and webrip and not interlaced:
-            log.info('webdl_1080p')
             if quality['identifier'] == 'webdl_1080p':
                 return True
 
         # bluray_720p
         elif r720 and rip and not interlaced:
-            log.info('bluray_720p')
             if quality['identifier'] == 'bluray_720p':
                 return True
 
         # bluray_1080p
         elif r1080 and rip and not interlaced:
-            log.info('bluray_1080p')
             if quality['identifier'] == 'bluray_1080p':
                 return True
 
