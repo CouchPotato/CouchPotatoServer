eea9f40501573c8554f0abc089bb0c8431fab7a3
Ruud <ruud@crashdummy.nl>
2014-12-19 09:01:52 +0100

    Use current

--- couchpotato/api.py
+++ couchpotato/api.py
@@ -34,7 +34,7 @@ def run_async(func):
 def run_handler(route, kwargs, callback = None):
     try:
         res = api[route](**kwargs)
-        IOLoop.instance().add_callback(callback, res, route)
+        IOLoop.current().add_callback(callback, res, route)
     except:
         log.error('Failed doing api request "%s": %s', (route, traceback.format_exc()))
         callback({'success': False, 'error': 'Failed returning results'}, route)
