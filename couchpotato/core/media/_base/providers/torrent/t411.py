9ca9ccdddce8512a3b6d3b9cc6d221ca99a7a355
Gaetan Cambier <gaetan@cambier.org>
2015-02-21 16:52:44 +0100

    Update t411.me to t411.io

--- couchpotato/core/media/_base/providers/torrent/t411.py
+++ couchpotato/core/media/_base/providers/torrent/t411.py
@@ -18,9 +18,9 @@ log = CPLog(__name__)
 class Base(TorrentProvider):
 
     urls = {
-        'test': 'http://www.t411.me/',
-        'detail': 'http://www.t411.me/torrents/?id=%s',
-        'search': 'http://www.t411.me/torrents/search/?',
+        'test': 'http://www.t411.io/',
+        'detail': 'http://www.t411.io/torrents/?id=%s',
+        'search': 'http://www.t411.io/torrents/search/?',
     }
 
     http_time_between_calls = 1 #seconds
@@ -120,8 +120,8 @@ class Base(TorrentProvider):
                                 testname=namer_check.correctName(name,movie)
                                 if testname==0:
                                     continue
-                                url = ('http://www.t411.me/torrents/download/?id=%s' % idt)
-                                detail_url = ('http://www.t411.me/torrents/?id=%s' % idt)
+                                url = ('http://www.t411.io/torrents/download/?id=%s' % idt)
+                                detail_url = ('http://www.t411.io/torrents/?id=%s' % idt)
                                 leecher = result.findAll('td')[8].text
                                 size = result.findAll('td')[5].text
                                 age = result.findAll('td')[4].text
@@ -185,7 +185,7 @@ class Base(TorrentProvider):
         ]
 
         try:
-            response = self.opener.open('http://www.t411.me/users/login/', self.getLoginParams())
+            response = self.opener.open('http://www.t411.io/users/login/', self.getLoginParams())
         except urllib2.URLError as e:
             log.error('Login to T411 failed: %s' % e)
             return False
@@ -234,7 +234,7 @@ config = [{
             'tab': 'searcher',
             'list': 'torrent_providers',
             'name': 't411',
-            'description': 'See <a href="https://www.t411.me/">T411</a>',
+            'description': 'See <a href="https://www.t411.io/">T411</a>',
             'icon' : 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAA3NCSVQICAjb4U/gAAACdklEQVQokW2RX0hTcRTHz+/+cbvz3m1srbv8M6Ws6SbK1hRTkUoKIui5jIJ8sz9vQQTRQxDRexCkIGgmSC+B1YNWNCIrRQ3Z2PyTf5pb2/S2ud2/2723hyIt/b4cDud7+H4OB2CXrpOW+wYLYPju0R66DTABEAWYB7i6lwHtbEYAKi5crPE36Wa6QGKQyYylk1cePPwX4FqPquSSiZVHAN+Gh/JihpezUpGXinmxkBN5Lvjm5U4/1hzwS5JsJIkzkWnmZDtSZF2WQZZ0SSoIgiSJXq+37VjLNhLL7h/ofUzg0Dceutl1ejHOoa0fScUQW1rouXQWw3ANULXbt8cNJ7pudPrcd/pmLp8PBNpa344HDYTqYc2Ls58G+59sI/0uTgBTKj78OQIdTb6W5gKg+PpKaPprUoLB/mBHY/v/CacARru7ucaG6NCrj5vp2rpDWvmBDa83PzDwdJVOl5Zo8S+JQhoD7E/CGMBEKLyYTNWjLKNl6KkP5OsXbE1leGqdNFoBd3K034jbcJzYfqfPTpUZjOHkmkmS+SpzinXYlxdGM+4I5ezkoyHSUcIjHXHY3wWPqM9SOg2ataFMlvQ6YWs5FIvaKxxgmzEfrWYOazanXuAxAGBwGALoNcWePxtx8cKR4wGuBFZo05TI2gXViE3SaiyVn3bQRgU0DABuVdHn7na6iuSMAOk2X6WnrqLcMVlqTVQ5lHw2VaQURtNN+7YoD7L4cQCQKGo9GJsUEGC6bNPfzc1xpZAjWuH7+3u+xHy+BuFLLkYsx7la0yrCAeqdZg0h1kDQFkpVlSyvrG1krM5mNbtK/9wM0wddjF6UNywElpWVX6HUDxDMdBkmAAAAAElFTkSuQmCC',
             'wizard': True,
             'options': [
