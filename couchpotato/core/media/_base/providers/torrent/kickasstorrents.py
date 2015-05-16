cc02d8e24653a33195afb059521e04f5a4762f43
Ruud <ruud@crashdummy.nl>
2015-05-16 22:20:30 +0200

    Remove KAT proxy

--- couchpotato/core/media/_base/providers/torrent/kickasstorrents.py
+++ couchpotato/core/media/_base/providers/torrent/kickasstorrents.py
@@ -32,11 +32,9 @@ class Base(TorrentMagnetProvider):
     proxy_list = [
         'https://kickass.to',
         'http://kickass.pw',
-        'http://kickassto.come.in',
         'http://katproxy.ws',
         'http://kickass.bitproxy.eu',
         'http://katph.eu',
-        'http://kickassto.come.in',
     ]
 
     def _search(self, media, quality, results):
