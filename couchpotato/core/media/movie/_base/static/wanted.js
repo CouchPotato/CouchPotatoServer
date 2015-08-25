915ef2ecc94b52ae3f8ef34ac42843bff8142897
Ruud <ruud@crashdummy.nl>
2015-08-25 21:30:04 +0200

    Nice userscript message on empty list

--- couchpotato/core/media/movie/_base/static/wanted.js
+++ couchpotato/core/media/movie/_base/static/wanted.js
@@ -35,7 +35,12 @@ var MoviesWanted = new Class({
 				'actions': [MA.IMDB, MA.Release, MA.Trailer, MA.Refresh, MA.Readd, MA.Delete, MA.Category, MA.Profile],
 				'add_new': true,
 				'menu': [self.manual_search, self.scan_folder],
-				'on_empty_element': App.createUserscriptButtons().addClass('empty_wanted')
+				'on_empty_element': new Element('div.empty_wanted').adopt(
+					new Element('div.no_movies', {
+						'text': 'Seems like you don\'t have any movies yet.. Maybe add some via search or the extension.'
+					}),
+					App.createUserscriptButtons()
+				)
 			});
 			$(self.list).inject(self.content);
 
