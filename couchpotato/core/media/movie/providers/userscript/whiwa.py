fbceb38d7df1e2a7ae5005c3397872acd084e8fe
Ruud <ruud@crashdummy.nl>
2015-08-25 14:29:06 +0200

    Userscript fixes and tests

--- couchpotato/core/media/movie/providers/userscript/whiwa.py
+++
@@ -1,8 +0,0 @@
-from couchpotato.core.media._base.providers.userscript.base import UserscriptBase
-
-autoload = 'WHiWA'
-
-
-class WHiWA(UserscriptBase):
-
-    includes = ['http://whiwa.net/stats/movie/*']
