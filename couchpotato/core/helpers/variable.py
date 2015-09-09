fb0ba3893e5c237e97a444b085abc35bb535d56e
Ruud <ruud@crashdummy.nl>
2015-09-09 17:37:29 +0200

    Log pyopenssl version

--- couchpotato/core/helpers/variable.py
+++ couchpotato/core/helpers/variable.py
@@ -411,3 +411,9 @@ def find(func, iterable):
             return item
 
     return None
+
+
+def compareVersions(version1, version2):
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    return cmp(normalize(version1), normalize(version2))
