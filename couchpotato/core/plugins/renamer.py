154adbc7df3638ab101689a8d042b4a7409283e0
Mark <rinseout@gmail.com>
2015-12-01 19:52:44 +0100

    Add renamer option to skip removing lower quality copies of a release

--- couchpotato/core/plugins/renamer.py
+++ couchpotato/core/plugins/renamer.py
@@ -505,19 +505,21 @@ class Renamer(Plugin):
                             group['meta_data']['quality'], {'identifier': release['quality'], 'is_3d': release.get('is_3d', False)}, profile, single = True)
 
                         if is_higher == 'higher':
-                            log.info('Removing lesser or not wanted quality %s for %s.', (media_title, release.get('quality')))
-                            for file_type in release.get('files', {}):
-                                for release_file in release['files'][file_type]:
-                                    remove_files.append(release_file)
-                            remove_releases.append(release)
+                            if self.conf('remove_lower_quality_copies'):
+                                log.info('Removing lesser or not wanted quality %s for %s.', (media_title, release.get('quality')))
+                                for file_type in release.get('files', {}):
+                                    for release_file in release['files'][file_type]:
+                                        remove_files.append(release_file)
+                                remove_releases.append(release)
 
                         # Same quality, but still downloaded, so maybe repack/proper/unrated/directors cut etc
                         elif is_higher == 'equal':
-                            log.info('Same quality release already exists for %s, with quality %s. Assuming repack.', (media_title, release.get('quality')))
-                            for file_type in release.get('files', {}):
-                                for release_file in release['files'][file_type]:
-                                    remove_files.append(release_file)
-                            remove_releases.append(release)
+                            if self.conf('remove_lower_quality_copies'):
+                                log.info('Same quality release already exists for %s, with quality %s. Assuming repack.', (media_title, release.get('quality')))
+                                for file_type in release.get('files', {}):
+                                    for release_file in release['files'][file_type]:
+                                        remove_files.append(release_file)
+                                remove_releases.append(release)
 
                         # Downloaded a lower quality, rename the newly downloaded files/folder to exclude them from scan
                         else:
@@ -1384,6 +1386,13 @@ config = [{
                     'type': 'bool',
                     'description': 'Cleanup leftover files after successful rename.',
                     'default': False,
+                },
+                {
+                    'name': 'remove_lower_quality_copies',
+                    'type': 'bool',
+                    'label': 'Delete Others',
+                    'description': 'Remove lower/equal quality copies of a release after downloading.',
+                    'default': True,
                 },
                 {
                     'advanced': True,
