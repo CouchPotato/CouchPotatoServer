dbcc4d007b8e2f2a64c5f4346b2a037099fe0cff
carl lambert <carl@inektech.com>
2016-11-06 15:53:24 +0000

    issue with none ascii chars in title name. decode to utf-8 and encode to ascii strips (for some reason the simple encode didn't work?

--- couchpotato/core/media/movie/providers/automation/base.py
+++ couchpotato/core/media/movie/providers/automation/base.py
@@ -45,7 +45,8 @@ class Automation(AutomationBase):
 
     def search(self, name, year = None, imdb_only = False):
 
-        prop_name = 'automation.cached.%s.%s' % (name, year)
+        cache_name = name.decode('utf-8').encode('ascii', 'ignore')
+        prop_name = 'automation.cached.%s.%s' % (cache_name, year)
         cached_imdb = Env.prop(prop_name, default = False)
         if cached_imdb and imdb_only:
             return cached_imdb
