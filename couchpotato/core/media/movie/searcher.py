32f22a48066d51c8ea06f446efa7e6eca746e99d
geogolem <geogolem@gmail.com>
2016-11-13 17:13:19 -0500

    fixed bug when searching for movie for first time check if dates is null

--- couchpotato/core/media/movie/searcher.py
+++ couchpotato/core/media/movie/searcher.py
@@ -181,14 +181,17 @@ class MovieSearcher(SearcherBase, MovieTypeBase):
             #define some options here
             #How should Netflix status effect searching/downloading
             #default: ignore netflix info and search/download regularly
-            netflixSearchEnabled = 1 
-            netflixDownloadEnabled = 1 #these options should be loaded from the configuration menu
+            netflixSearchEnabled = 0 
+            netflixDownloadEnabled = 0 #these options should be loaded from the configuration menu
             #default behavior is both are enabled i.e. netflix info is only displayed but has no effect
             #the below code assumes that netflixSearchEnabled and netflixDownloadEnabled
             #   are loaded from the config file
             netflixDownloadEnabled = netflixDownloadEnabled and netflixSearchEnabled #dont download if no search
 
-            movieOnNetflix = release_dates.get('netflix') >0
+            if release_dates:
+                movieOnNetflix = release_dates.get('netflix')
+            else:
+                movieOnNetflix = 0
             log.debug('Checking if the movie is available on Netflix')
             if (movieOnNetflix):
                 log.debug('The movie is available on Netflix')
