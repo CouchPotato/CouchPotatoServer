9d6dd023c6ede4bc7e582e833b47859fe7636559
geogolem <geogolem@gmail.com>
2016-11-16 01:02:18 -0500

    if the year of the movie is in the future, its not on netflix

--- couchpotato/core/media/movie/providers/info/couchpotatoapi.py
+++ couchpotato/core/media/movie/providers/info/couchpotatoapi.py
@@ -143,7 +143,7 @@ class CouchPotatoApi(MovieProvider):
         #other countries are supported by allflicks.net, so other country codes could be added
         #but the session heads must be adjusted accordingly or proper JSON wont be returned
         #firefox extension tamper is useful for determining country code/appropriate headers for a country
-        now_year = str(date.today().year)
+        now_year = date.today().year
 
         url = 'https://www.allflicks.net/wp-content/themes/responsive/processing/processing_%s.php?draw=9&columns[0][data]=box_art&columns[0][name]=&columns[0][searchable]=true&columns[0][orderable]=false&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=title&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=true&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=year&columns[2][name]=&columns[2][searchable]=true&columns[2][orderable]=true&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=genre&columns[3][name]=&columns[3][searchable]=true&columns[3][orderable]=true&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=rating&columns[4][name]=&columns[4][searchable]=true&columns[4][orderable]=true&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=available&columns[5][name]=&columns[5][searchable]=true&columns[5][orderable]=true&columns[5][search][value]=&columns[5][search][regex]=false&columns[6][data]=director&columns[6][name]=&columns[6][searchable]=true&columns[6][orderable]=true&columns[6][search][value]=&columns[6][search][regex]=false&columns[7][data]=cast&columns[7][name]=&columns[7][searchable]=true&columns[7][orderable]=true&columns[7][search][value]=&columns[7][search][regex]=false&order[0][column]=5&order[0][dir]=desc&start=%s&length=%s&search[value]=%s&search[regex]=false&movies=true&shows=false&documentaries=true&rating=netflix&min=1900&max=%s&_=1478945015662'
         log.debug('-------------->querying Allflicks for title: %s' %title)
@@ -153,18 +153,18 @@ class CouchPotatoApi(MovieProvider):
         length=100
         start=0
         numFound = 1 #this just forces at least one execution of the following loop
-        while start < numFound:
+        while start < numFound and not year > now_year:
             with requests.Session() as session:
                 session.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0"}
                 if countryCode=='ca':
                     session.get("http://www.allflicks.net/canada/")
-                    response = session.get(url % (countryCode,str(start),str(length),title,now_year),headers={"Accept" : "application.json, text/javascript, */*; q=0.01",
+                    response = session.get(url % (countryCode,str(start),str(length),title,str(now_year)),headers={"Accept" : "application.json, text/javascript, */*; q=0.01",
                                                   "X-Requested-With": "XMLHttpRequest",
                                                   "Referer": "http://www.allflicks.net/canada/",
                                                   "Host": "www.allflicks.net"})
                 else:
                     session.get("http://www.allflicks.net/") 
-                    response = session.get(url % (countryCode,str(start),str(length),title,now_year),headers={"Accept" : "application/json, text/javascript, */*; q=0.01", 
+                    response = session.get(url % (countryCode,str(start),str(length),title,str(now_year)),headers={"Accept" : "application/json, text/javascript, */*; q=0.01", 
                                                  "X-Requested-With": "XMLHttpRequest", 
                                                  "Referer": "http://www.allflicks.net/", 
                                                  "Host": "www.allflicks.net"})
@@ -175,7 +175,7 @@ class CouchPotatoApi(MovieProvider):
             if start == 0: log.debug('----->Movie %s returned %s results from Allflicks' % (title,numFound))
             results = j1['data']
             for result in results:
-                if result['title'] == title:
+                if result['title'].upper() == title.upper():   #this needs to be case insensitive
                     log.debug('---> Movie %s matched with %s from Allflicks' % (title,result['title']))
                     p='%d %b %Y'
                     ndate=int(time.mktime(time.strptime(result['available'],p)))
